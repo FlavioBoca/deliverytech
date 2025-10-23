8e85f1c53eea3f1519db4170e16d17ae9fb35fb9
FlavioBoca <flavio.boca@gmail.com>
2025-10-22 22:30:35 -0300

    Atualizações dos scripts para implementação do swagger - login, permissões e JWT.

---
+++ src/main/java/com/deliverytech/delivery_api/model/Usuario.java
@@ -0,0 +1,80 @@
+package com.deliverytech.delivery_api.model;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String Nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean ativo;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "restaurante_id")
+    private Long restauranteId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(role);
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return ativo;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return ativo;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return ativo;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ativo;
+    }
+
+}
